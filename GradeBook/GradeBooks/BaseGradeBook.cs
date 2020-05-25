61ecaac0185b04dff58f421b510c312dc17612a1
James Calder <64359068+james-a-calder@users.noreply.github.com>
2020-05-25 16:46:55 +0100

    7 - Add support for weighted grading

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,13 +14,16 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
+            IsWeighted = isWeighted;
             Students = new List<Student>();
         }
 
         public GradeBookType Type { get; set; }
+
+        public bool IsWeighted { get; set; }
 
         public void AddStudent(Student student)
         {
@@ -108,20 +111,32 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            var points = 0;
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    points = 4;
+                    break;
                 case 'B':
-                    return 3;
+                    points = 3;
+                    break;
                 case 'C':
-                    return 2;
+                    points = 2;
+                    break;
                 case 'D':
-                    return 1;
+                    points = 1;
+                    break;
                 case 'F':
-                    return 0;
+                    points = 0;
+                    break;
             }
-            return 0;
+
+            if (IsWeighted)
+            {
+                if (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled)
+                    points++;
+            }
+            return points;
         }
 
         public virtual void CalculateStatistics()
@@ -265,7 +280,7 @@ namespace GradeBook.GradeBooks
                              from type in assembly.GetTypes()
                              where type.FullName == "GradeBook.GradeBooks.StandardGradeBook"
                              select type).FirstOrDefault();
-            
+
             return JsonConvert.DeserializeObject(json, gradebook);
         }
     }
